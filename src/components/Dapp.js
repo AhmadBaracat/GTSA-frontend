0661a92296426c225b513ac4885a9488eba255ca
Baracat <baracat@aucegypt.edu>
2022-05-21 14:51:29 +0200

    Remove connect wallet button to see if it will fix the ssl sha2 issue

--- src/components/Dapp.js
+++ src/components/Dapp.js
@@ -75,15 +75,15 @@ export class Dapp extends React.Component {
     //
     // Note that we pass it a callback that is going to be called when the user
     // clicks a button. This callback just calls the _connectWallet method.
-    if (!this.state.selectedAddress) {
-      return (
-        <ConnectWallet
-          connectWallet={() => this._connectWallet()}
-          networkError={this.state.networkError}
-          dismiss={() => this._dismissNetworkError()}
-        />
-      );
-    }
+    // if (!this.state.selectedAddress) {
+    //   return (
+    //     <ConnectWallet
+    //       connectWallet={() => this._connectWallet()}
+    //       networkError={this.state.networkError}
+    //       dismiss={() => this._dismissNetworkError()}
+    //     />
+    //   );
+    // }
 
     // If the token data or the user's balance hasn't loaded yet, we show
     // a loading component.
